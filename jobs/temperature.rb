2e06025ff95225335ca4a7ae47555bdfe0e3e9c9
Felix Schober <felix.schober@tum.de>
2017-12-31 17:18:13 +0100

    fix wrong api path for temperature

--- jobs/temperature.rb
+++ jobs/temperature.rb
@@ -1,5 +1,5 @@
 def get_current_temperature(http, location)
-	request = create_request("/api/hue/motion/#{location}/temperature")
+	request = create_request("/api/hue/sensors/#{location}/temperature")
   	response = http.request(request)
  	data = JSON.parse(response.body)
  	return data['temperature']
@@ -13,6 +13,6 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
 	http = create_http
 
 	last_temperature_kitchen = current_temperature_kitchen
-	current_power_level = get_current_temperature(http, 'kitchen')
+	current_temperature_kitchen = get_current_temperature(http, 'kitchen')
 	send_event('currentTemperature', { current: current_temperature_kitchen, last: last_temperature_kitchen })
 end
